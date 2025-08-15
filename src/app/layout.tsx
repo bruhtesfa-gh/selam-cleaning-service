eeaab87191144ae1018847b50de654abf2b7b535
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2025-08-15 17:32:49 +0300

    refactor: Comment out import of Metadata for cleaner layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils"; // Assuming you have a cn utility
